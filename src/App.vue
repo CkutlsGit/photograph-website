1b6a83c65ed95659d6b557cb77ecf59c937283ef
Kostya <ckutls228@gmail.com>
2025-03-20 16:02:12 +0300

    added adaptive and all style for type-photograph block

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,23 @@
 <script setup lang="ts">
+import { reactive } from "vue"
+
 import TheHeader from "./components/TheHeader.vue"
 import BaseButton from "./components/BaseButton.vue"
+
+interface ITypePhotograph {
+  name: string
+  price: number
+  img: string
+}
+
+const typeArray: Array<ITypePhotograph> = reactive([
+  { name: "Свадебная фотосессия", img: "src/assets/img/1.png", price: 3000 },
+  { name: "Фотографии беременных", img: "src/assets/img/6.png", price: 3000 },
+  { name: "Детская фотосъемка", img: "src/assets/img/3.png", price: 3000 },
+  { name: "Семейная фотосессия", img: "src/assets/img/4.png", price: 3000 },
+  { name: "Фотографии еды", img: "src/assets/img/2.png", price: 3000 },
+  { name: "Эстрадная фотосъемка", img: "src/assets/img/5.png", price: 3000 },
+])
 </script>
 
 <template>
@@ -30,19 +47,32 @@ import BaseButton from "./components/BaseButton.vue"
       </footer>
     </div>
   </main>
-  <section class="type-photograph">
-    <div class="type-photograph__content">
+  <section class="type-photograph mt-32">
+    <div class="type-photograph__content max-w-[1280px] mx-auto text-center">
       <header class="type-photograph__content-title">
-        <h1 class="text-2xl font-bold">Виды съемок</h1>
+        <h1 class="text-xl text-[#EA3546] font-medium max-sm:text-base">Виды съемок</h1>
       </header>
       <article class="type-photograph__content-description">
-        <h1 class="text-4xl font-bold">Закажите подходящую</h1>
-        <p>
+        <h1 class="text-4xl font-bold my-6 max-sm:text-3xl max-sm:my-2">Закажите подходящую</h1>
+        <p class="text-xl font-medium max-w-3xl mx-auto max-sm:max-w-xl max-sm:text-base">
           Посмотрите примеры фотографий и обратитесь ко мне за тем видом съемки,
           который вам необходим
         </p>
       </article>
-      <footer class="type-photograph__content-items">
+      <footer class="type-photograph__content-items mt-16 justify-center">
+        <ul class="flex justify-center flex-wrap gap-8 text-center font-bold">
+          <li v-for="(type, index) in typeArray" :key="index" class="relative">
+            <div class="relative w-96 h-96 max-sm:w-72 max-sm:h-72">
+              <h1 class="absolute inset-0 text-white text-2xl mt-72 max-sm:mt-52 max-sm:text-xl">
+                {{ type.name }}
+              </h1>
+              <h2 class="absolute inset-0 text-white text-xl mt-82 max-sm:mt-60 max-sm:text-base">
+                от {{ type.price }} ₽
+              </h2>
+              <img class="w-full h-full" :src="type.img" alt="" />
+            </div>
+          </li>
+        </ul>
       </footer>
     </div>
   </section>

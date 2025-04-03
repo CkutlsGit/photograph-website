a59fc9157f0c21c51569efb7bdd54bd07e1f4bf2
Kostya <ckutls228@gmail.com>
2025-04-03 04:19:21 +0300

    changed style code with prettier

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import type { IBase } from "./interfaces"
 import TheHeader from "./components/header/TheHeader.vue"
 import BaseButton from "./components/base/BaseButton.vue"
 
-const phoneNumber = ref<string>('')
+const phoneNumber = ref<string>("")
 
 interface ITypePhotograph extends IBase {
   price: number
@@ -20,21 +20,45 @@ interface IFeedback extends IAdvantage {
 }
 
 interface IPriceAdvantage {
-  name: string,
-  price: number,
+  name: string
+  price: number
   advantages: {
-    name: string,
+    name: string
     isAdvantage: boolean
   }[]
 }
 
 const typeArray: Array<ITypePhotograph> = reactive([
-  { name: "Свадебная фотосессия", icon: "src/assets/img/base/types-photo/1.png", price: 3000 },
-  { name: "Фотографии беременных", icon: "src/assets/img/base/types-photo/6.png", price: 3000 },
-  { name: "Детская фотосъемка", icon: "src/assets/img/base/types-photo/3.png", price: 3000 },
-  { name: "Семейная фотосессия", icon: "src/assets/img/base/types-photo/4.png", price: 3000 },
-  { name: "Фотографии еды", icon: "src/assets/img/base/types-photo/2.png", price: 3000 },
-  { name: "Эстрадная фотосъемка", icon: "src/assets/img/base/types-photo/5.png", price: 3000 },
+  {
+    name: "Свадебная фотосессия",
+    icon: "src/assets/img/base/types-photo/1.png",
+    price: 3000,
+  },
+  {
+    name: "Фотографии беременных",
+    icon: "src/assets/img/base/types-photo/6.png",
+    price: 3000,
+  },
+  {
+    name: "Детская фотосъемка",
+    icon: "src/assets/img/base/types-photo/3.png",
+    price: 3000,
+  },
+  {
+    name: "Семейная фотосессия",
+    icon: "src/assets/img/base/types-photo/4.png",
+    price: 3000,
+  },
+  {
+    name: "Фотографии еды",
+    icon: "src/assets/img/base/types-photo/2.png",
+    price: 3000,
+  },
+  {
+    name: "Эстрадная фотосъемка",
+    icon: "src/assets/img/base/types-photo/5.png",
+    price: 3000,
+  },
 ])
 
 const advantagesArray: Array<IAdvantage> = reactive([
@@ -70,42 +94,97 @@ const advantagesArray: Array<IAdvantage> = reactive([
   },
 ])
 
-  const feedbacksArray: Array<IFeedback> = reactive([
-    { name: 'Юрий Ставничий', icon: 'src/assets/img/icons/feedbacks/user-yuriy.svg', description: 'Это супер крутая инструкция и вы делаете очень полезную вещь. Спасибо', jobUser: 'Фотограф' },
-    { name: 'Александра Селезнева', icon: 'src/assets/img/icons/feedbacks/user-alexandra.svg', description: 'Работа с сервисом приносит радость, потому что высочайший профессионализм, удобство, оперативность, и просто приятные люди!', jobUser: 'Фотограф' },
-    { name: 'Оля Павлова', icon: 'src/assets/img/icons/feedbacks/user-olga.svg', description: 'Спасибо большое за помощь и содействие!', jobUser: 'Фотограф' },
-  ])
+const feedbacksArray: Array<IFeedback> = reactive([
+  {
+    name: "Юрий Ставничий",
+    icon: "src/assets/img/icons/feedbacks/user-yuriy.svg",
+    description:
+      "Это супер крутая инструкция и вы делаете очень полезную вещь. Спасибо",
+    jobUser: "Фотограф",
+  },
+  {
+    name: "Александра Селезнева",
+    icon: "src/assets/img/icons/feedbacks/user-alexandra.svg",
+    description:
+      "Работа с сервисом приносит радость, потому что высочайший профессионализм, удобство, оперативность, и просто приятные люди!",
+    jobUser: "Фотограф",
+  },
+  {
+    name: "Оля Павлова",
+    icon: "src/assets/img/icons/feedbacks/user-olga.svg",
+    description: "Спасибо большое за помощь и содействие!",
+    jobUser: "Фотограф",
+  },
+])
 
-  const priceArray: Array<IPriceAdvantage> = reactive([
-    { name: 'Базовая съемка', price: 5000, advantages: [
-      { name: 'Полная обработка всех фотографий в ручную (от 250)', isAdvantage: true },
-      { name: 'Предварительная консультация и планирование', isAdvantage: true },
-      { name: 'Авторская обработка фотографий', isAdvantage: true },
-      { name: 'Запись на USB-флешку в индивидуальном вудбоксе', isAdvantage: false },
-      { name: 'Хранение фото на сервере', isAdvantage: false },
-      { name: 'WEB-версия для интернета', isAdvantage: false },
-      { name: 'Два фотографа, ассистент', isAdvantage: false },
-    ] },
-    { name: 'Оптимальная съемка', price: 8999, advantages: [
-      { name: 'Полная обработка всех фотографий в ручную (от 250)', isAdvantage: true },
-      { name: 'Предварительная консультация и планирование', isAdvantage: true },
-      { name: 'Авторская обработка фотографий', isAdvantage: true },
-      { name: 'Запись на USB-флешку в индивидуальном вудбоксе', isAdvantage: true },
-      { name: 'Хранение фото на сервере', isAdvantage: true },
-      { name: 'WEB-версия для интернета', isAdvantage: false },
-      { name: 'Два фотографа, ассистент', isAdvantage: false },
-    ] },
-    { name: 'Премиум съемка', price: 15000, advantages: [
-      { name: 'Полная обработка всех фотографий в ручную (от 250)', isAdvantage: true },
-      { name: 'Предварительная консультация и планирование', isAdvantage: true },
-      { name: 'Авторская обработка фотографий', isAdvantage: true },
-      { name: 'Запись на USB-флешку в индивидуальном вудбоксе', isAdvantage: true },
-      { name: 'Хранение фото на сервере', isAdvantage: true },
-      { name: 'WEB-версия для интернета', isAdvantage: true },
-      { name: 'Два фотографа, ассистент', isAdvantage: true },
-    ] },
-    
-  ])
+const priceArray: Array<IPriceAdvantage> = reactive([
+  {
+    name: "Базовая съемка",
+    price: 5000,
+    advantages: [
+      {
+        name: "Полная обработка всех фотографий в ручную (от 250)",
+        isAdvantage: true,
+      },
+      {
+        name: "Предварительная консультация и планирование",
+        isAdvantage: true,
+      },
+      { name: "Авторская обработка фотографий", isAdvantage: true },
+      {
+        name: "Запись на USB-флешку в индивидуальном вудбоксе",
+        isAdvantage: false,
+      },
+      { name: "Хранение фото на сервере", isAdvantage: false },
+      { name: "WEB-версия для интернета", isAdvantage: false },
+      { name: "Два фотографа, ассистент", isAdvantage: false },
+    ],
+  },
+  {
+    name: "Оптимальная съемка",
+    price: 8999,
+    advantages: [
+      {
+        name: "Полная обработка всех фотографий в ручную (от 250)",
+        isAdvantage: true,
+      },
+      {
+        name: "Предварительная консультация и планирование",
+        isAdvantage: true,
+      },
+      { name: "Авторская обработка фотографий", isAdvantage: true },
+      {
+        name: "Запись на USB-флешку в индивидуальном вудбоксе",
+        isAdvantage: true,
+      },
+      { name: "Хранение фото на сервере", isAdvantage: true },
+      { name: "WEB-версия для интернета", isAdvantage: false },
+      { name: "Два фотографа, ассистент", isAdvantage: false },
+    ],
+  },
+  {
+    name: "Премиум съемка",
+    price: 15000,
+    advantages: [
+      {
+        name: "Полная обработка всех фотографий в ручную (от 250)",
+        isAdvantage: true,
+      },
+      {
+        name: "Предварительная консультация и планирование",
+        isAdvantage: true,
+      },
+      { name: "Авторская обработка фотографий", isAdvantage: true },
+      {
+        name: "Запись на USB-флешку в индивидуальном вудбоксе",
+        isAdvantage: true,
+      },
+      { name: "Хранение фото на сервере", isAdvantage: true },
+      { name: "WEB-версия для интернета", isAdvantage: true },
+      { name: "Два фотографа, ассистент", isAdvantage: true },
+    ],
+  },
+])
 </script>
 
 <template>
@@ -210,20 +289,29 @@ const advantagesArray: Array<IAdvantage> = reactive([
   <section class="order mt-28">
     <div class="order__content text-center">
       <header class="order__title">
-        <h1 class="text-5xl font-bold max-md:text-3xl">Закажите съемку прямо сейчас</h1>
+        <h1 class="text-5xl font-bold max-md:text-3xl">
+          Закажите съемку прямо сейчас
+        </h1>
         <p class="font-semibold max-w-3xl mx-auto mt-4 max-md:max-w-xl">
           Я проконсультирую Вас по основным вопросам подготовки к фотосессии, а
           также расскажу о современных тенденциях в фотографии.
         </p>
       </header>
       <article class="order__input mt-10">
-        <MaskInput class="input-style mr-4 rounded-lg max-sm:mb-4" v-model="phoneNumber" placeholder="+7 (000) 000-0000" mask="+# (###) ###-####"/>
+        <MaskInput
+          class="input-style mr-4 rounded-lg max-sm:mb-4"
+          v-model="phoneNumber"
+          placeholder="+7 (000) 000-0000"
+          mask="+# (###) ###-####"
+        />
         <BaseButton color="text-white" bgColor="bg-light-orange-700"
           >Оставить заявку
         </BaseButton>
       </article>
       <footer class="order__info mt-6 max-sm:mt-2">
-        <p class="text-transparent-black-light font-semibold max-w-lg mx-auto max-sm:text-xs">
+        <p
+          class="text-transparent-black-light font-semibold max-w-lg mx-auto max-sm:text-xs"
+        >
           Оставляя заявку, вы соглашаетесь на
           <span>обработку персональных данных</span> и с
           <span>условиями бронирования счёта</span>
@@ -238,45 +326,80 @@ const advantagesArray: Array<IAdvantage> = reactive([
         <h2 class="text-xl font-medium">Настоящие отзывы моих клиентов</h2>
       </header>
       <article class="feedbacks__list mt-20">
-        <ul class="flex justify-center gap-6 max-w-[1110px] mx-auto max-xl:flex-col">
-          <li class="bg-white border-transparent-black rounded-2xl w-96 h-full mx-auto mb-4 px-8 py-10 max-sm:w-64" v-for="(feedback, index) in feedbacksArray" :key="index">
+        <ul
+          class="flex justify-center gap-6 max-w-[1110px] mx-auto max-xl:flex-col"
+        >
+          <li
+            class="bg-white border-transparent-black rounded-2xl w-96 h-full mx-auto mb-4 px-8 py-10 max-sm:w-64"
+            v-for="(feedback, index) in feedbacksArray"
+            :key="index"
+          >
             <div class="flex justify-center">
-              <img :src="feedback.icon" :alt="`Фото пользователя - ${ feedback.name }`">
+              <img
+                :src="feedback.icon"
+                :alt="`Фото пользователя - ${feedback.name}`"
+              />
             </div>
             <p class="text-2xl font-bold max-w-72 mx-auto my-8 max-sm:text-lg">
               {{ feedback.description }}
             </p>
             <h2 class="text-lg font-medium mb-1">{{ feedback.name }}</h2>
-            <h3 class="text-xs text-transparent-black-light font-bold">{{ feedback.jobUser }}</h3>
+            <h3 class="text-xs text-transparent-black-light font-bold">
+              {{ feedback.jobUser }}
+            </h3>
           </li>
         </ul>
       </article>
     </div>
   </section>
-  <section class="price bg-[linear-gradient(#662E9B66,#662E9B66),url('../img/backgrounds/background-image-price.jpg')] bg-cover bg-center min-h-[800px] pt-20">
+  <section
+    class="price bg-[linear-gradient(#662E9B66,#662E9B66),url('../img/backgrounds/background-image-price.jpg')] bg-cover bg-center min-h-[800px] pt-20"
+  >
     <div class="price__content text-center">
       <header class="price__title text-white">
         <h1 class="text-5xl font-bold mb-6 max-sm:text-3xl">Прайс-лист</h1>
         <h2 class="text-xl">Цены и услуги свадебного фотографа</h2>
       </header>
       <article class="price__list mt-16">
-        <ul class="flex justify-center gap-6 max-w-[1110px] mx-auto max-xl:flex-col max-xl:items-center">
-          <li class="text-left bg-white border-transparent-black rounded-2xl w-96 mb-4 p-8 max-sm:max-w-64" v-for="(priceType, index) in priceArray" :key="index">
-            <h3 class="text-light-orange-700 font-medium mb-2.5">{{ priceType.name }}</h3>
+        <ul
+          class="flex justify-center gap-6 max-w-[1110px] mx-auto max-xl:flex-col max-xl:items-center"
+        >
+          <li
+            class="text-left bg-white border-transparent-black rounded-2xl w-96 mb-4 p-8 max-sm:max-w-64"
+            v-for="(priceType, index) in priceArray"
+            :key="index"
+          >
+            <h3 class="text-light-orange-700 font-medium mb-2.5">
+              {{ priceType.name }}
+            </h3>
             <h2 class="text-2xl font-bold">{{ priceType.price }} ₽</h2>
             <div class="bg-transparent-black w-full h-px my-4"></div>
             <div>
               <ul>
-                <li class="flex items-center" v-for="(advantage, index) in priceType.advantages" :key="index">
-                  <img v-if="advantage.isAdvantage" src="/src/assets/img/icons/price/price-icon-checkmark.svg">
-                  <img v-else src="/src/assets/img/icons/price/price-icon-cross.svg">
+                <li
+                  class="flex items-center"
+                  v-for="(advantage, index) in priceType.advantages"
+                  :key="index"
+                >
+                  <img
+                    v-if="advantage.isAdvantage"
+                    src="/src/assets/img/icons/price/price-icon-checkmark.svg"
+                  />
+                  <img
+                    v-else
+                    src="/src/assets/img/icons/price/price-icon-cross.svg"
+                  />
                   <p class="max-w-64 m-1">
                     {{ advantage.name }}
                   </p>
                 </li>
               </ul>
               <div class="flex justify-center mt-6 animation-hover">
-                <button class="text-white bg-light-orange-700 w-full py-3.5 rounded-4xl cursor-pointer">Выбрать</button>
+                <button
+                  class="text-white bg-light-orange-700 w-full py-3.5 rounded-4xl cursor-pointer"
+                >
+                  Выбрать
+                </button>
               </div>
             </div>
           </li>

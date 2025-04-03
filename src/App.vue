f7dbce1316eb45809b9a1fc453f34913e483acfd
Kostya <ckutls228@gmail.com>
2025-04-03 04:06:04 +0300

    fixed grid for elements in price (grid feedbacks-price)

--- src/App.vue
+++ src/App.vue
@@ -238,7 +238,7 @@ const advantagesArray: Array<IAdvantage> = reactive([
         <h2 class="text-xl font-medium">Настоящие отзывы моих клиентов</h2>
       </header>
       <article class="feedbacks__list mt-20">
-        <ul class="flex justify-center gap-6 max-w-[1280px] mx-auto max-xl:flex-col">
+        <ul class="flex justify-center gap-6 max-w-[1110px] mx-auto max-xl:flex-col">
           <li class="bg-white border-transparent-black rounded-2xl w-96 h-full mx-auto mb-4 px-8 py-10 max-sm:w-64" v-for="(feedback, index) in feedbacksArray" :key="index">
             <div class="flex justify-center">
               <img :src="feedback.icon" :alt="`Фото пользователя - ${ feedback.name }`">
@@ -259,20 +259,22 @@ const advantagesArray: Array<IAdvantage> = reactive([
         <h1 class="text-5xl font-bold mb-6 max-sm:text-3xl">Прайс-лист</h1>
         <h2 class="text-xl">Цены и услуги свадебного фотографа</h2>
       </header>
-      <article class="price__list mt-8">
-        <ul class="flex justify-center gap-8 max-xl:flex-col max-xl:items-center">
-          <li class="text-left bg-white border-transparent-black rounded-2xl mb-4 p-8 max-xl:max-w-96 max-sm:max-w-64" v-for="(priceType, index) in priceArray" :key="index">
+      <article class="price__list mt-16">
+        <ul class="flex justify-center gap-6 max-w-[1110px] mx-auto max-xl:flex-col max-xl:items-center">
+          <li class="text-left bg-white border-transparent-black rounded-2xl w-96 mb-4 p-8 max-sm:max-w-64" v-for="(priceType, index) in priceArray" :key="index">
             <h3 class="text-light-orange-700 font-medium mb-2.5">{{ priceType.name }}</h3>
             <h2 class="text-2xl font-bold">{{ priceType.price }} ₽</h2>
             <div class="bg-transparent-black w-full h-px my-4"></div>
             <div>
-              <li class="flex items-center" v-for="(advantage, index) in priceType.advantages" :key="index">
-                <img v-if="advantage.isAdvantage" src="/src/assets/img/icons/price/price-icon-checkmark.svg">
-                <img v-else src="/src/assets/img/icons/price/price-icon-cross.svg">
-                <p class="max-w-64 m-1">
-                  {{ advantage.name }}
-                </p>
-              </li>
+              <ul>
+                <li class="flex items-center" v-for="(advantage, index) in priceType.advantages" :key="index">
+                  <img v-if="advantage.isAdvantage" src="/src/assets/img/icons/price/price-icon-checkmark.svg">
+                  <img v-else src="/src/assets/img/icons/price/price-icon-cross.svg">
+                  <p class="max-w-64 m-1">
+                    {{ advantage.name }}
+                  </p>
+                </li>
+              </ul>
               <div class="flex justify-center mt-6 animation-hover">
                 <button class="text-white bg-light-orange-700 w-full py-3.5 rounded-4xl cursor-pointer">Выбрать</button>
               </div>
